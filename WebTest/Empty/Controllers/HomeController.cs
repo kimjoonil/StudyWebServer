0d6ad0f1148e5706ca9b11dcdb2f08745722c2d4
Kimjoonil <joonilone@naver.com>
2023-04-07 16:36:18 +0900

    2023-04-07 Studing Webserver

---
+++ WebTest/Empty/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Empty.Models;
+
+namespace Empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MessageTest msg = new MessageTest()
+            {
+                Message = "Welcome to Asp.Net MVC !"
+            };
+
+            ViewBag.Noti = "input message and click submit";
+
+            return View(msg);
+        }
+
+        //POST를 처리하는 index
+        [HttpPost]
+        public IActionResult Index(MessageTest obj)
+        {
+            ViewBag.Noti = "Message Changed";
+            return View(obj);
+        }
+    }
+}
---
+++ WebTest/Empty/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empty
+{
+    //MVC =  Model View Controller  Model 데이터  View 인테리어 Controller 액션 행동
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
