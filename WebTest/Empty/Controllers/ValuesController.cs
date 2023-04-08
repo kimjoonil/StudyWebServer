7016c7b88cb8cb27d6aa70866eafba0490f7cc25
Kimjoonil <joonilone@naver.com>
2023-04-08 19:25:53 +0900

    2023-04-08 Studying WebServer

---
+++ WebTest/Empty/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Empty.Models;
+
+namespace Empty.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<MessageTest> Get()
+        {
+            List<MessageTest> messages = new List<MessageTest>();
+            messages.Add(new MessageTest() { Message = "Hello Web API 1!" });
+            messages.Add(new MessageTest() { Message = "Hello Web API 2!" });
+            messages.Add(new MessageTest() { Message = "Hello Web API 3!" });
+
+            return messages;
+        }
+    }
+}
